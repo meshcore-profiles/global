d6e2cfdbf2ec5e0ddc3865324797af5738e9823a
Sefinek <contact@sefinek.net>
2026-09-12 08:15:24 +0200

    Exit on fatal MongoDB error

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,9 +1,15 @@
 const { connect, connection } = require('mongoose');
 
-connect(process.env.MONGODB_URL).catch(err => console.error('Failed to connect to the database', err));
+connect(process.env.MONGODB_URL).catch(err => {
+	console.error('Failed to connect to the database', err);
+	process.exit(1);
+});
 
 connection.on('connected', () => console.log('Connected to the database successfully'));
 connection.on('disconnected', () => console.warn('MongoDB disconnected!'));
-connection.on('error', err => console.error('MongoDB error:', err.message));
+connection.on('error', err => {
+	console.error('MongoDB error:', err.message);
+	process.exit(1);
+});
 
 module.exports = connection;
